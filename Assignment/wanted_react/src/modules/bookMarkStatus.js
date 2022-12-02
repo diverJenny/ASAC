00e0233dc70252d547bb6270bf5c9ceab1d19b3f
diverJenny <jenny.es2005@gmail.com>
2022-12-02 17:13:29 +0900

    20221202 bookmark 기능구현

--- Assignment/wanted_react/src/modules/bookMarkStatus.js
+++ Assignment/wanted_react/src/modules/bookMarkStatus.js
@@ -2,29 +2,26 @@
 
 // 1. action type 설정(액션 이름은 대문자로)
 const ADD_BOOKMARK = "bookMarkStatus/ADD_BOOKMARK";
-const DELETE_BOOKMARK = "bookMarkStatus/DELETE_BOOKMARK";
 
 // 2. action 함수 생성
-export const addBookMark = () => ({
+export const addBookMark = (id) => ({
   type: ADD_BOOKMARK,
-});
-
-export const deleteBookMark = () => ({
-  type: DELETE_BOOKMARK,
+  id: id
 });
 
 // 3. 초기 상태 설정
-const initialState = {
-  bookMarks: [],
-};
+const initialState = [];
 
 // 4. 받은 상태에 따른 분기처리
 function bookMarkStatus(state = initialState, action) {
   switch (action.type) {
     case ADD_BOOKMARK:
-      return {
-        ...state,
-        [...bookMarks ],
-    };
+      if (state.includes(action.id))
+        return state.filter((element) => element !== action.id);
+      else return [...state , action.id];
+    default:
+      return state;
   }
 }
+
+export default bookMarkStatus;
